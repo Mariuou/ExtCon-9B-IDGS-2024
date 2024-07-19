b33eb7e68b6cd81dc6c86698d5f6686c2973f352
Dulce Esmeralda <163442061+DulceSme@users.noreply.github.com>
2024-07-19 14:40:31 -0600

    Add files via upload

---
+++ Unidad-3/210542_scripts.sql
@@ -0,0 +1,19 @@
+-- PRÁCTICA 13: REVISIÓN Y CORRECCIÓN DE LAS TABLAS:
+-- Servicios al Cliente, Servicios Sucursales, Instalaciones, Equipamiento y Ejercicios
+
+-- Elaborado por: Dulce Esmeralda Hernández Juárez 210542
+-- Grado y Grupo: 9° B
+-- Programa Educativo: Ingeniería de Desarrollo y Gestión de Software
+-- Fecha elaboración: 19 de Julio de 2024
+
+-- Tabla: Servicios al Cliente
+
+-- a) Revisión de la composición de la tabla
+
+-- b) Cambios sugeridos
+
+-- c) Revisión de la Población Estática (corrección en caso de ser necesaria)
+
+-- d) Revisión de los 4 triggers (AFTER INSERT, BEFORE UPDATE, AFTER UPDATE, AFTER DELETE)
+
+-- e) Realizar una consulta join (en caso de que aplique) para comprobar la integridad de la información.
