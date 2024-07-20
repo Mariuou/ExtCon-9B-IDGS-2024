fde4d01de7ce1f13021ac45cd51a5487a84ff137
Lorena <lorenaascencion2003@gmail.com>
2024-07-19 21:04:05 -0600

    Edición de archivos

---
+++ Unidad 3/210885_scripts.sql
@@ -0,0 +1,10 @@
+-- PRACTICA 13: REVISIÓN Y CORRECCIÓN DE LAS TABLAS:
+-- SERVICIOS AL CLIENTE, SERVICIOS SUCURSALES, INSTALACIONES, EQUIPAMIENTO Y EJERCICIOS
+
+-- ELABORADO POR: Maria Lorena Ascencion Andres
+--GRADO Y GRUPO: 9°'B'
+--PROGRAMA EDUCATIVO: Ingeniería en Desarrollo y Gestión de Software
+-- FECHA ELABORACIÓN: 19 de julio de 2024
+
+-- TABLA: tbc_servicios_clientes
+-- a) Revision de la composición de la tabla
